4720c6bbeaa97d7e35a43aeb2b9638fbf0a047eb
Chi Hung Hsieh <adamare0000@gmail.com>
2024-10-05 19:37:32 +0800

    try communicating between server and client

--- server.c
+++ server.c
@@ -182,8 +182,8 @@ int main(int argc, char** argv) {
                 }
 
 #ifdef READ_SERVER      
-                sprintf(buf,"%s : %s",accept_read_header,requestP[conn_fd].buf);
-                write(requestP[conn_fd].conn_fd, buf, strlen(buf));
+                sprintf(buf,"%s : %s",accept_read_header,requestP[fds[i].fd].buf);
+                write(requestP[fds[i].fd].conn_fd, buf, strlen(buf));
 #elif defined WRITE_SERVER
                 sprintf(buf,"%s : %s",accept_write_header,requestP[conn_fd].buf);
                 write(requestP[conn_fd].conn_fd, buf, strlen(buf));    
